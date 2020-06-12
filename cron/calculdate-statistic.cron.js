eaed7ecdde631f64b4a2a49ca3be1e0887df951b
MariaNegrych <negrychmariya@gmail.com>
2020-06-12 11:24:37 +0300

    hw-8

--- cron/calculdate-statistic.cron.js
+++ cron/calculdate-statistic.cron.js
@@ -1,7 +1,17 @@
-const {userService} = require('../service')
+const fs = require('fs-extra').promises;
+const path = require('path');
+
+const {userService, productService} = require('../service')
 
 module.exports = async () => {
-    let newVar = await userService.getAllUsers();
+    let usersNumber = await userService.getAllUsers();
+    let productNumber = await productService.getAllProducts();
 
-    console.log(newVar.length)
+    console.log(usersNumber.length, productNumber.length);
+
+    await fs.mkdir(path.resolve(process.cwd(), 'statistic'), {recursive: true});
+
+    fs.writeFile(path.join(process.cwd(), 'statistic', 'users.txt'), `${usersNumber.length}`);
+
+    fs.writeFile(path.join(process.cwd(), 'statistic', 'products.txt'), `${productNumber.length}`);
 }
